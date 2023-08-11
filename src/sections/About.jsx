149a451031bffd1a0b9627dc1bb5d3988c7f18b4
Dylan Koevort <koevortd@gmail.com>
2023-08-11 22:40:50 +0200

    Added Dark Mode feature

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -61,34 +61,34 @@ const About = () => {
 	];
 
 	return (
-		<section id="about" className="border-b-2 border-b-white bg-[#050815]">
-			<div className="max-w-[1240px] mx-auto w-full py-12 px-8 md:px-16 p-4 pb-16">
-				<h2 className="text-5xl pb-4 text-[#c1c2c9] font-bold">About Me</h2>
+		<section id="about" className="dark:border-b-2 dark:border-b-white  bg-light-whiteOne dark:bg-dark-primary duration-500">
+			<div className="max-w-[1240px] mx-auto w-full py-12 px-8 md:px-16 p-4 pb-16 border-b-2 border-light-secondary dark:border-none">
+				<h2 className="text-5xl pb-4 text-light-darkGreyTwo dark:text-dark-secondary font-bold">About Me</h2>
 				<div className="grid grid-cols-1 gap-y-6 md:grid-cols-2 ">
-					<div className="p-1 lg:p-8 sm:p-4">
-						<h2 className="text-white text-md sm:text-xl pb-8 ">The languages I've spoken</h2>
-						<div className="grid grid-cols-4 gap-x-4 gap-y-8 sm:gap-y-16 sm:gap-x-8 text-white text-sm">
+					<div className="p-1 lg:p-8 sm:p-4 pb-8">
+						<h2 className="text-md sm:text-xl font-medium pb-8">The languages I've spoken</h2>
+						<div className="grid grid-cols-4 gap-x-4 gap-y-8 sm:gap-y-16 sm:gap-x-8">
 							{iconsLanguages.map((icon) => (
 								<div
 									key={icon.label}
-									className="flex align-center flex-col text-center h-[60px] w-[60px] lg:h-[90px] lg:w-[90px] md:h-[60px] md:w-[60px] sm:h-[90px] sm:w-[90px] mx-auto hover:scale-110 transition-transform"
+									className="flex align-center flex-col text-center h-[60px] w-[60px] lg:h-[70px] lg:w-[70px] md:h-[60px] md:w-[60px] sm:h-[70px] sm:w-[70px] mx-auto hover:scale-110 transition-transform"
 								>
 									<img src={icon.image} alt={icon.label} />
-									<p className="text-md lg:text-xl">{icon.label}</p>
+									<p className="text-md">{icon.label}</p>
 								</div>
 							))}
 						</div>
 					</div>
 					<div className="p-1 lg:p-8 sm:p-4">
-						<h2 className="text-white text-md sm:text-xl pb-8">The tools I've used</h2>
-						<div className="grid grid-cols-4 gap-x-4 gap-y-8 sm:gap-y-16 sm:gap-x-8 text-white text-sm">
+						<h2 className="text-md sm:text-xl font-medium pb-8">The tools I've used</h2>
+						<div className="grid grid-cols-4 gap-x-4 gap-y-8 sm:gap-y-16 sm:gap-x-8">
 							{iconsTools.map((icon) => (
 								<div
 									key={icon.label}
-									className="flex align-center flex-col text-center h-[60px] w-[60px] lg:h-[90px] lg:w-[90px] md:h-[60px] md:w-[60px] sm:h-[90px] sm:w-[90px] mx-auto hover:scale-110 transition-transform"
+									className="flex align-center flex-col text-center h-[60px] w-[60px] lg:h-[70px] lg:w-[70px] md:h-[60px] md:w-[60px] sm:h-[70px] sm:w-[70px] mx-auto hover:scale-110 transition-transform"
 								>
 									<img src={icon.image} alt={icon.label} />
-									<p className="text-md lg:text-xl">{icon.label}</p>
+									<p className="text-md">{icon.label}</p>
 								</div>
 							))}
 						</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	darkMode: 'class',
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				dark: {
+					primary: '#050815', // dark blue
+					secondary: '#f8f9fa', // whiteish grey colour
+					tertiary: '#405DE6', // lighter blue
+					darkGreyOne: '#121212',
+					darkGreyTwo: '#333333',
+
+					highlightBg: colors.gray[900],
+					highlightText: colors.red[400]
+				},
+				light: {
+					primary: '#f8f9fa',
+					secondary: '#050815',
+					tertiary: '#405DE6',
+
+					whiteOne: '#fafcff',
+					greyOne: '#eeeeee',
+					greyTwo: '#dddddd',
+					greyThree: '#cccccc',
+					darkGreyOne: '#121212',
+					darkGreyTwo: '#333333',
+
+					highlightBg: colors.gray[900],
+					highlightText: colors.red[400]
+				}
+			}
+		}
 	},
 	plugins: [require('@tailwindcss/forms')]
 };
