e28d9c1b65c073cf27c8eb2c86321a88a29656c8
Dylan Koevort <koevortd@gmail.com>
2023-08-15 18:31:20 +0200

    Added Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { Navbar, Footer } from 'components';
 import { useEffect } from 'react';
 import { Hero, About, Projects, Contact } from 'sections';
+import { logEvent } from 'firebase/analytics';
+import { analytics } from './firebase';
 
 const App = () => {
+	useEffect(() => {
+		logEvent(analytics, 'page_view');
+	});
 	return (
 		<div className="text-light-secondary dark:text-dark-secondary ease-in-out duration-500">
 			<Navbar />
